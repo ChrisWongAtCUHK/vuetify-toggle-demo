54857d70583ac46a5e85b79bb6bde7a1ee9fdca1
Chris Wong <wc38.wong@gmail.com>
2025-02-04 16:16:46 +0800

    Expandable Table

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,87 @@
 <template>
-  <HelloWorld />
+  <v-data-table
+    v-model:expanded="expanded"
+    :headers="dessertHeaders"
+    :items="desserts"
+    item-value="name"
+    show-expand
+  >
+    <template v-slot:top>
+      <v-toolbar flat>
+        <v-toolbar-title>Expandable Table</v-toolbar-title>
+      </v-toolbar>
+    </template>
+    <template v-slot:expanded-row="{ columns, item }">
+      <tr>
+        <td :colspan="columns.length">More info about {{ item.name }}</td>
+      </tr>
+    </template>
+  </v-data-table>
 </template>
 
-<script setup>
-  //
+<script>
+export default {
+  data() {
+    return {
+      expanded: [],
+      dessertHeaders: [
+        {
+          title: "Dessert (100g serving)",
+          align: "start",
+          sortable: false,
+          key: "name",
+        },
+        { title: "Calories", key: "calories" },
+        { title: "Fat (g)", key: "fat" },
+        { title: "Carbs (g)", key: "carbs" },
+        { title: "Protein (g)", key: "protein" },
+        { title: "Iron (%)", key: "iron" },
+        { title: "", key: "data-table-expand" },
+      ],
+      desserts: [
+        {
+          name: "Frozen Yogurt",
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          iron: 1,
+        },
+        {
+          name: "Ice cream sandwich",
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          iron: 1,
+        },
+        {
+          name: "Eclair",
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+          iron: 7,
+        },
+        {
+          name: "Gingerbread",
+          calories: 356,
+          fat: 16.0,
+          carbs: 49,
+          protein: 3.9,
+          iron: 16,
+        },
+
+        {
+          name: "KitKat",
+          calories: 518,
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+          iron: 6,
+        },
+      ],
+    };
+  },
+};
 </script>
